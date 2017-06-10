d3eba2a6df3008e5bc8ba42c571d156b072d071b
Hydra <hydragg@gmail.com>
2017-06-10 21:13:39 +0800

    add C2F App : temperature conversion

---
+++ src/components/C2F.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+  .c2f
+    h3 Celsius: {{ celsius }} °C
+    h3 Fahrenheit {{ fahrenheig }} °F
+    .temperature-input
+      input(v-model.number="celsius" placeholder="Celsius")
+    //- .selector
+    //-   input(type="radio" id="c2f" value="c2f" v-model="mode")
+    //-   label(for="c2f") C2F
+    //-   input(type="radio" id="f2c" value="f2c" v-model="mode")
+    //-   label(for="f2c") F2C
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        celsius: 0,
+        //mode: 'c2f'
+      }
+    },
+    computed: {
+      fahrenheig() {
+        return this.celsius * 9/5 + 32;
+      }
+    }
+  }
+</script>
+
+<style>
+  
+</style>
