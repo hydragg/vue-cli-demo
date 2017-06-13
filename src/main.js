be47cd647a91be9d852eea0e9fe75e69099b6d91
Hydra <hydragg@gmail.com>
2017-06-13 22:11:46 +0800

    add vuex

--- src/main.js
+++ src/main.js
@@ -3,13 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import vuex from 'vuex'
+import Vuex from 'vuex'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue)
-Vue.use(vuex)
+Vue.use(Vuex)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
